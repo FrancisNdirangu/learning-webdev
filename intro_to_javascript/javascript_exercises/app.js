90a0002c05ee99d1e677810dc9eab658a16b7859
frankngugi <frankngugi@gmail.com>
2026-03-29 22:33:31 +0530

    added some slicing exercise to the code

--- intro_to_javascript/javascript_exercises/app.js
+++ intro_to_javascript/javascript_exercises/app.js
@@ -1,3 +1,5 @@
 var text = prompt('Write your tweet')
 var char = text.length
+var sli = text.slice(0,140)
 console.log('You have made '+char+' number of characters')
+console.log('The first 140 characters are::'+sli)
