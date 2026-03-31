521146c96b0270d9fa3a775bdcd27ea66f827932
frankngugi <frankngugi@gmail.com>
2026-03-31 18:44:41 +0530

    using conditionals and if statements to create a bmi calculator

--- intro_to_javascript/javascript_exercises/app.js
+++ intro_to_javascript/javascript_exercises/app.js
@@ -9,20 +9,43 @@
 
 // console.log('First letter to be uppercased '+text.slice(0,1).toUpperCase())
 
-function lifeInWeeks(age) {
+// function lifeInWeeks(age) {
     
-/************Don't change the code above************/    
+// /************Don't change the code above************/    
     
-    //Write your code here.
-    var years = 90-age;
-    var months = years*12;
-    var weeks = years*52;
-    var days = years*365;
-    console.log('You have '+days+' days, '+weeks+' weeks, and '+months+' months left.');
-    return 'You have this many years left:'+years; 
+//     //Write your code here.
+//     var years = 90-age;
+//     var months = years*12;
+//     var weeks = years*52;
+//     var days = years*365;
+//     console.log('You have '+days+' days, '+weeks+' weeks, and '+months+' months left.');
+//     return 'You have this many years left:'+years; 
     
     
-/*************Don't change the code below**********/
+// /*************Don't change the code below**********/
+// }
+// var francisLife = lifeInWeeks(27)
+// console.log(francisLife)
+
+
+function bmiCalculator (weight, height) {
+    
+    var BMI = weight/(height**2)
+    
+    if (BMI > 24.9){
+        var interpretation = 'Your BMI is '+BMI+ ', so you are overweight.';
+        console.log(interpretation)
+    }
+    if (BMI >18.5 && BMI<24.9){
+         interpretation = 'Your BMI is '+BMI+', so you have a normal weight.';
+        console.log(interpretation)
+        }
+    if (BMI <18.5) {
+         interpretation = 'Your BMI is '+BMI+', so you are underweight.';
+        console.log(interpretation)
+        }
+    
+    return interpretation;
 }
-var francisLife = lifeInWeeks(27)
-console.log(francisLife)
+
+bmiCalculator(80,1.82)
