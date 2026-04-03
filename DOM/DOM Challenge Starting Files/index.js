00fe011a19df2f08762a0199d11d91ad7fe015e6
frankngugi <frankngugi@gmail.com>
2026-04-03 19:32:29 +0530

    used javascript to toggle on and off the css classList huge in javascript

--- DOM/DOM Challenge Starting Files/index.js
+++ DOM/DOM Challenge Starting Files/index.js
@@ -1,1 +1,11 @@
 document.querySelector('.list a').style.color = 'red'
+
+document.querySelector('li').classList.toggle('huge')
+
+
+document.querySelector('li').classList.toggle('huge')
+
+document.querySelector('li').classList.toggle('huge')
+
+document.querySelector('li').classList.toggle('huge')
+
