633265381f245d9357f95918c8d3a87b37e8be9a
frankngugi <frankngugi@gmail.com>
2026-04-04 20:32:52 +0530

    written code so that there is a response to clicking any of the buttons

--- advanced_js_and_dom_manipulation/Drum Kit Starting Files/index.js
+++ advanced_js_and_dom_manipulation/Drum Kit Starting Files/index.js
@@ -1,6 +1,8 @@
 
-document.querySelector('button').addEventListener('click',clickResponse);
-
+var button_index = [0,1,2,3,4,5,6]; 
+for (const i of button_index){
+document.querySelectorAll('.drum')[i].addEventListener('click',clickResponse);
+}
 function clickResponse() {
     alert('I got clicked');
 }
