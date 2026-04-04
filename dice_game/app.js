4dae4c45cc1c8deafc5d3041f84e5fecaaae62e7
frankngugi <frankngugi@gmail.com>
2026-04-04 19:46:19 +0530

    wrote the code to change the title to state the winner of the dice game

--- dice_game/app.js
+++ dice_game/app.js
@@ -9,4 +9,12 @@ var list_dice = ['dice1.png','dice2.png','dice3.png','dice4.png','dice5.png','di
 var dice1Path = 'images/'+list_dice[randomNumber1];
 var dice2Path = 'images/'+list_dice[randomNumber2];
 document.querySelector('.dice .img1').src = dice1Path;
-document.querySelector('.dice .img2').src = dice2Path; 
+document.querySelector('.dice .img2').src = dice2Path;
+
+if (randomNumber1>randomNumber2) {
+    document.querySelector('h1').textContent = 'Player 1 wins!';
+} else if (randomNumber2>randomNumber1) {
+    document.querySelector('h1').textContent = 'Player 2 wins!';
+} else {
+    document.querySelector('h1').textContent = 'Draw';
+}
