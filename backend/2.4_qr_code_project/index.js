1c90f3b56e2f66f9ee5e48a0bc1525f3216cb429
frankngugi <frankngugi@gmail.com>
2026-04-07 20:36:08 +0530

    used node js to get an input from the user and then generate an svg qrcode

--- backend/2.4_qr_code_project/index.js
+++ backend/2.4_qr_code_project/index.js
@@ -3,3 +3,46 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+// import QRCode from 'qrcode'
+// import writeFile from 'fs'
+// import readline from 'readline'
+// var rl = readline.createInterface({
+//     input:process.stdin,
+//     output:process.stdout
+// });
+
+// var response = rl.question('enter your input: ', (name) =>{
+//     console.log(`Hello, ${name}`);
+//     rl.close();
+// });
+
+// QRCode.create(response)
+
+// // qrcodeCreator(response);
+
+// writeFile('user_input_file.txt',response,(err) =>{
+//     if (err) throw err;
+//     console.log('user input file created');
+// });
+
+import { input } from '@inquirer/prompts';
+import fs from 'node:fs';
+import qr from 'qr-image';
+
+const answer = await input({message: 'Enter your input: '});
+
+// var qr = require('qr-image');
+// var qr_svg = qr.image(answer,{type:'svg'});
+// var qr = require('qr-image');
+ 
+var qr_svg = qr.image('I love QR!', { type: 'svg' });
+qr_svg.pipe(fs.createWriteStream('custom_qr_code.svg'))
+
+// var fileWriter = writeFile()
+
+fs.writeFile('user_input_file.txt',answer,(err)=>{
+    if (err) throw err;
+    console.log('user input file created');
+});
+
+
