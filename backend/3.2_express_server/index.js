d5711d4a2e5d3c519d213a8fdea25384e9595b4c
frankngugi <frankngugi@gmail.com>
2026-04-08 16:44:33 +0530

    in express 3.2 i started another server used the get request with different endpoints

--- backend/3.2_express_server/index.js
+++ backend/3.2_express_server/index.js
@@ -5,8 +5,16 @@ const app = express();
 const port = 5000;
 
 app.get('/',(req,res) => {
-    res.send('Hello World');
+    res.send('<h1>Hello<h1>');
 });
+
+app.get('/contact',(req,res) => {
+    res.send('<h2>My contact is tralalalala');
+});
+
+app.get('/about',(req,res) => {
+    res.send('<h3>I am 6 foot tall</h3>')
+})
 
 app.listen(port, () => {
     console.log(`Port is running on port ${port}`);
