4dbeebf0b264353431a83d4f4a494a002b72af01
frankngugi <frankngugi@gmail.com>
2026-04-09 17:27:28 +0530

    made a folder for section 4 for ejs in backend, also made a function for returning the type of day it is

---
+++ backend/4.0_ejs/views/index.js
@@ -0,0 +1,24 @@
+
+//function to give the day of the week based of input of date
+
+var dayOfWeek = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday',];
+// var dayType = ['weekend','weekday'];
+function dateToday(date) {
+    
+    const enteredDay = new Date(date);
+    const dayNumber = enteredDay.getDay();
+    console.log(dayNumber);
+
+    if (dayNumber>0 & dayNumber<6) {
+        var dayType = 'Weekday';
+        // console.log(dayType);
+        return dayType;
+    } else {
+        var dayType = 'Weekend';
+        // console.log(dayType);
+        return dayType;
+    }
+};
+
+var x = dateToday('09-04-2026');
+console.log(x);
