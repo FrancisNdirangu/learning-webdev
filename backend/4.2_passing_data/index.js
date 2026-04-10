c2250289bd2c37cffb726280bb98fbc945f670eb
frankngugi <frankngugi@gmail.com>
2026-04-10 17:06:38 +0530

    managed to successfully get information from the html form in ejs to the server expressJS file and then use that information to perform a calculation and send the result back to the EJS

--- backend/4.2_passing_data/index.js
+++ backend/4.2_passing_data/index.js
@@ -6,9 +6,21 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render('index.ejs');
+  // console.log(req.body['First Name'])
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  // console.log(req.body['fName']);
+  // console.log(req.body['lName']);
+  const firstName = req.body['fName'];
+  const lastName = req.body['lName'];
+  var lengthName= firstName.length + lastName.length;
+  console.log(lengthName);
+  res.locals = {lenName : lengthName}
+  res.render('index.ejs')
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
