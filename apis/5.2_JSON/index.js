c30e3fc13588f81f6b838b1662a81a4d8ca31748
frankngugi <frankngugi@gmail.com>
2026-04-24 21:23:54 +0530

    wrote code that extracts the name of the item clicked on

--- apis/5.2_JSON/index.js
+++ apis/5.2_JSON/index.js
@@ -18,6 +18,10 @@ app.get("/", (req, res) => {
 
 app.post("/recipe", (req, res) => {
   //Step 3: Write your code here to make this behave like the solution website.
+  const buttonValue = req.body['choice'];
+  console.log(buttonValue);
+
+
   //Step 4: Add code to views/index.ejs to use the recieved recipe object.
 });
 
