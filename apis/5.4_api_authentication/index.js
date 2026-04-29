a4c8b08a90af1bc5fbc601bde65d92d6398da5d7
frankngugi <frankngugi@gmail.com>
2026-04-29 23:58:37 +0530

    made the /noAuth (req,res) function an async function then also added the try and cath error code

--- apis/5.4_api_authentication/index.js
+++ apis/5.4_api_authentication/index.js
@@ -15,13 +15,17 @@ app.get("/", (req, res) => {
   res.render("index.ejs", { content: "API Response." });
 });
 
-app.get("/noAuth", (req, res) => {
+app.get("/noAuth", async (req, res) => {
   //TODO 2: Use axios to hit up the /random endpoint
   //The data you get back should be sent to the ejs file as "content"
   //Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
 
+  try {
   const request = await axios.get(API_URL+'random');
-  console.log(request)
+  console.log(request);
+  } catch (error) {
+    console.error(error.message);
+  }
 
 });
 
