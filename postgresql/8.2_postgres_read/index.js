7126f3a44a2598e50bdeb33cf88d9e0a03d2f6b8
frankngugi <frankngugi@gmail.com>
2026-06-01 15:02:13 +0300

    defined the postgres database client so it knows what database its connected to

--- postgresql/8.2_postgres_read/index.js
+++ postgresql/8.2_postgres_read/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let currentQuestion = {};
+
+const db = new pg.Client(
+  {   user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:DB_NAME,
+    password:DB_PASSWORD,
+    port:DB_PORT
+  }
+);
 
 // GET home page
 app.get("/", (req, res) => {
