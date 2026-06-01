516a50b99ca7cf88461f960c4d5f8c3ab14b17aa
frankngugi <frankngugi@gmail.com>
2026-06-01 18:28:41 +0300

    imported the dotenv and pg

--- postgresql/8.3_travel_tracker/index.js
+++ postgresql/8.3_travel_tracker/index.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+import pg from 'pg';
 import express from "express";
 import bodyParser from "body-parser";
 
@@ -9,6 +11,9 @@ app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+  //I will have to pass the variable with the key = countries
+  //pass key called total that has the count of the number of countries len(array)
+  //
 });
 
 app.listen(port, () => {
