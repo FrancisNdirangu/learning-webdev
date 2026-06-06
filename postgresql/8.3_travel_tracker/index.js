e0c8b281de9cdeb87c3818c3391dec1fa4c896a6
unknown <frankngugi@gmail.com>
2026-06-06 12:43:23 +0300

    added the render and other list creation steps into the query to ensure the data exists to load onto the map. this was done in the get(/) endpoint

--- postgresql/8.3_travel_tracker/index.js
+++ postgresql/8.3_travel_tracker/index.js
@@ -1,5 +1,5 @@
-import 'dotenv/config';
-import pg from 'pg';
+import "dotenv/config";
+import pg from "pg";
 import express from "express";
 import bodyParser from "body-parser";
 
@@ -9,32 +9,30 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const db = new pg.Client(
-  {   user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT 
-  });
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 db.connect();
 
-let countries_visited = [];
+//let countries_visited = [];
 
-db.query("SELECT * FROM visited_countries", (err,res) => {
-  if (err) {
-    console.error("Not executed the query", err.stack);
-  } else {
-    countries_visited = res.rows;
-  }
-});
-
+// db.query("SELECT * FROM visited_countries", (err,res) => {
+//   if (err) {
+//     console.error("Not executed the query", err.stack);
+//   } else {
+//     countries_visited = res.rows;
+//   }
+// });
 
 //let list_countries = []
 // list_countries = countries_visited.forEach((element) => {
 //   list_countries.push(element['countries_code']);
 // });
-
 
 app.get("/", async (req, res) => {
   //Write your code here.
@@ -43,37 +41,55 @@ app.get("/", async (req, res) => {
   //
   //console.log(countries_visited);
 
-  const list_countries = countries_visited.map((item) => item.countries_code);
+  const all_visited_countries = await db.query(
+    "SELECT * FROM visited_countries",
+    (err, result) => {
+      if (err) {
+        console.error(
+          "Failed to execute the query in visited countries table",
+          err.stack,
+        );
+      } else {
+        const countries_visited = result.rows;
+        const list_countries = countries_visited.map(
+          (item) => item.countries_code,
+        );
 
-  const total = countries_visited.length;
-  //console.log(list_countries);
-  res.render("index.ejs",{countries:list_countries,total:total});
-
+        const total = countries_visited.length;
+        //console.log(list_countries);
+        res.render("index.ejs", { countries: list_countries, total: total });
+      }
+    },
+  );
+  //console.log(countries_visited);
 });
 
-let countries_and_codes_db = []
-db.query("SELECT * FROM countries", (err,res) => {
-    if (err) {
-      console.error("Failed to make query to countries table", err.stack);
-    } else {
-      countries_and_codes_db = res.rows;
-    }
-  });
+let countries_and_codes_db = [];
+db.query("SELECT * FROM countries", (err, res) => {
+  if (err) {
+    console.error("Failed to make query to countries table", err.stack);
+  } else {
+    countries_and_codes_db = res.rows;
+  }
+});
 
-
-app.post("/add", (req,res) => {
-
+app.post("/add", (req, res) => {
   const list_countries = countries_visited.map((item) => item.countries_code);
   const total = countries_visited.length;
   const input_country = req.body.country;
   // console.log(input_country);
   // console.log(countries_and_codes_db);
-  const input_country_code = countries_and_codes_db.find(element => element.country_name.trim().toLowerCase() == input_country.trim().toLowerCase());
+  const input_country_code = countries_and_codes_db.find(
+    (element) =>
+      element.country_name.trim().toLowerCase() ==
+      input_country.trim().toLowerCase(),
+  );
   console.log(input_country_code);
-  const countries_and_codes_list = countries_and_codes_db.map((item) => item.country_code);
+  const countries_and_codes_list = countries_and_codes_db.map(
+    (item) => item.country_code,
+  );
   // console.log(countries_and_codes_list);
 });
-
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
